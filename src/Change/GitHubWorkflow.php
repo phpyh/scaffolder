fc63ff2bba8d029af9fa0340f0baab26ecdc0fa6
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2025-12-04 05:58:45 +0300

    Calculate ci matrix based on php versions

--- src/Change/GitHubWorkflow.php
+++ src/Change/GitHubWorkflow.php
@@ -5,22 +5,36 @@ declare(strict_types=1);
 namespace PHPyh\Scaffolder\Change;
 
 use PHPyh\Scaffolder\Change;
+use PHPyh\Scaffolder\Cli;
 use PHPyh\Scaffolder\Fact\PackageType;
+use PHPyh\Scaffolder\Fact\PhpImageVersions;
 use PHPyh\Scaffolder\Fact\Project;
 use PHPyh\Scaffolder\Facts;
 
 enum GitHubWorkflow implements Change
 {
     case Change;
+    private const string FILE = '.github/workflows/check.yaml';
 
     public function decide(Facts $facts, Project $project): ?callable
     {
-        return new CopyFile(
-            origin: \sprintf(
-                __DIR__ . '/../../files/%s_check.yaml',
-                $facts[PackageType::class] === PackageType::LIBRARY ? 'library' : 'project',
-            ),
-            target: '.github/workflows/check.yaml',
-        )->decide($facts, $project);
+        $contents = $project->read(\sprintf(
+            __DIR__ . '/../../files/%s_check.yaml',
+            $facts[PackageType::class] === PackageType::LIBRARY ? 'library' : 'project',
+        ));
+        $contents = str_replace(
+            "'%matrix%'",
+            \sprintf("['%s']", implode("', '", $facts[PhpImageVersions::class])),
+            $contents,
+        );
+
+        if ($project->tryRead(self::FILE) === $contents) {
+            return null;
+        }
+
+        return static fn(Cli $cli) => $cli->step(
+            \sprintf('Write `%s`...', self::FILE),
+            static fn() => $project->write(self::FILE, $contents),
+        );
     }
 }
