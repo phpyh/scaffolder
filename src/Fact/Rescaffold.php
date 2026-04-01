905e03ee15b774b6098122741c015981931615d4
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-02 01:18:40 +0300

    Add Rescaffold<bool> fact

---
+++ src/Fact/Rescaffold.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPyh\Scaffolder\Fact;
+
+use PHPyh\Scaffolder\Cli;
+use PHPyh\Scaffolder\Fact;
+use PHPyh\Scaffolder\Facts;
+
+/**
+ * @extends Fact<bool>
+ */
+final class Rescaffold extends Fact
+{
+    public static function resolve(Facts $facts, Cli $cli): mixed
+    {
+        return !$facts[Project::class]->exists('composer.json');
+    }
+}
